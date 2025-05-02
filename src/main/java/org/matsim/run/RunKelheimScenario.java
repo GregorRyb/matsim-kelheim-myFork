489ee0b89b236ba2ed0699df08cc53ea4465f27c
GregorRyb <rybczak@vsp.tu-berlin.de>
2025-05-02 15:22:10 +0200

    preprae teaching WIP

--- src/main/java/org/matsim/run/RunKelheimScenario.java
+++ src/main/java/org/matsim/run/RunKelheimScenario.java
@@ -16,6 +16,7 @@ import org.matsim.api.core.v01.events.PersonScoreEvent;
 import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.application.MATSimApplication;
@@ -52,6 +53,7 @@ import org.matsim.core.config.groups.RoutingConfigGroup;
 import org.matsim.core.config.groups.VspExperimentalConfigGroup;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.router.AnalysisMainModeIdentifier;
 import org.matsim.core.scoring.functions.ScoringParametersForPerson;
 import org.matsim.drtFare.KelheimDrtFareModule;
@@ -284,6 +286,9 @@ public class RunKelheimScenario extends MATSimApplication {
 			}
 		}
 
+
+		addNetworkModifications(scenario.getNetwork());
+
 	}
 
 	@Override
@@ -382,4 +387,51 @@ public class RunKelheimScenario extends MATSimApplication {
 
 		}
 	}
+
+	/*
+	Simple method to reduece the free speed on all links where cars are allowed.
+	 */
+	private void addNetworkModifications(Network network) {
+		//parse the network links
+		for (Link link: network.getLinks().values()) {
+			//filter for car links
+			if (link.getAllowedModes().contains(TransportMode.car)) {
+				//set free speed to 10 m/s
+				link.setFreespeed(10.0);
+			}
+		}
+	}
+
+
+	/*
+	Simple method to add a highway to the scenario.
+	 */
+	private void addHighwayToTheNetwork(Network network) {
+
+		//get the
+		Node fromNode = network.getNodes().get("29999218");
+		Node toNode = network.getNodes().get("370357925");
+
+		Id<Link> linkIdfMyNewHighway = Id.createLinkId("myNewHighway");
+		double lengthOfMyNewHighway = NetworkUtils.getEuclideanDistance(fromNode.getCoord(), toNode.getCoord());
+		double freeSpeedOfMyNewHighway = 120.0 / 3.6;
+		double capacity = 2000;
+		double numberOfLanesOfMyNewHighway = 1.0;
+
+		Link myNewLink = NetworkUtils.createLink(linkIdfMyNewHighway,
+			fromNode,
+			toNode,
+			network,
+			lengthOfMyNewHighway,
+			freeSpeedOfMyNewHighway,
+			capacity,
+			numberOfLanesOfMyNewHighway);
+
+		network.addLink(myNewLink);
+
+
+
+
+	}
+
 }
